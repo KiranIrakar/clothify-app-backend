3d12e9de719879ce5695a8539d855ab434b058c6
Imran Maniyar <imranmaniyar419@gmail.com>
2026-03-28 18:40:47 +0530

    added cros in app.ts

--- app.ts
+++ app.ts
@@ -1,12 +1,16 @@
 import Fastify from "fastify";
 import dotenv from "dotenv";
+import fastifyCors from "@fastify/cors";
 import authRoutes from "./src/routes/auth.route";
 import productRoutes from "./src/routes/product.route";
 import { sequelize } from "./src/config/db";
 dotenv.config();
 
 export const app = Fastify();
-
+   app.register(fastifyCors, {
+  origin: "http://localhost:4200",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+});
 // routes register
 app.register(authRoutes, { prefix: "/auth" });
 app.register(productRoutes, { prefix: "/products" });

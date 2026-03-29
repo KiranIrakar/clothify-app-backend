f2d10dd4fffb7f516b358b2ff84603ae3d615b11
Kiran Irakar <irakarkiran0@gmail.com>
2026-03-29 12:56:13 +0530

    initial commit

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -4,19 +4,11 @@ import { authMiddleware } from "../middlewares/auth.middleware";
 
 export default async function authRoutes(fastify: FastifyInstance) {
 
-  const authController = new AuthController(); 
-
-  fastify.post(
-    "/signup",
-    { preHandler: [authMiddleware] }, 
-    authController.signup
-  );
-
+  const authController = new AuthController();
+  
+  fastify.post("/signup", /* { preHandler: [authMiddleware] }, */ authController.signup);
   fastify.post("/verify-otp", authController.verifyOtp);
-
   fastify.post("/resend-otp", authController.resendOtp);
-
   fastify.post("/reset-password", authController.resetPassword);
-
-  fastify.post("/generate-otp", authController.generateOtp);
-}
+  fastify.post("/generate-otp", authController.generateOtp); 
+}  

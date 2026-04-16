b3fe4af9304f2e9d8241c8bd35fc92baa97f75fe
Kiran Irakar <irakarkiran0@gmail.com>
2026-04-16 17:02:36 +0530

    fix logger issue

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-// import "./src/utils/whatsapp";
-// import "./src/utils/whatsapp";
+
 import { app } from "./app";
 import sequelize from "./src/config/db";
 import { logger } from "./src/utils/logger";
@@ -10,17 +9,17 @@ const start = async () => {
   try {
     // DB connect
     await sequelize.sync();
-    logger.info("Database connected");
-     app.log.info("Database connected successfully");
+    logger.info("Database connected successfully");
 
-   const port = Number(process.env.PORT) || 3000;
-   
+    const port = Number(process.env.PORT) || 3000;          
     // Server start
     await app.listen({
-      port: Number(process.env.PORT) || 3000,
+      port,
       host: "0.0.0.0",
     });
-    logger.info("Server running", { port: Number(process.env.PORT) || 3000 });
+
+    app.log.info(`Server is running at http://localhost:${port}`);
+
   } catch (err) {
     logger.error("Error starting server", err);
     process.exit(1);

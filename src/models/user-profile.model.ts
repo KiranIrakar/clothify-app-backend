9773e5c11eb8f812ccd291d7d4a8285545a19d8a
Imran Maniyar <imranmaniyar419@gmail.com>
2026-04-16 14:59:56 +0530

    remove users table

--- src/models/user-profile.model.ts
+++ src/models/user-profile.model.ts
@@ -10,11 +10,18 @@ class UserProfile
   implements UserProfileAttributes
 {
   public id!: string;
-  public fullName!: string;
-  public email!: string;
-  public mobileNumber!: string;
+  public fullName!: string | null;
+  public name!: string | null;
+  public email!: string | null;
+  public mobileNumber!: string | null;
+  public phone!: string | null;
   public password!: string | null;
   public isVerified!: boolean;
+  public enabled!: boolean;
+  public role!: "ROLE_USER" | "ROLE_ADMIN";
+  public otp!: string | null;
+  public otp_expiry!: Date | number | null;
+  public temprory_phone!: string | null;
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
 }
@@ -28,11 +35,16 @@ UserProfile.init(
     },
     fullName: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      field: "fullName",
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
       unique: true,
       validate: {
         isEmail: true,
@@ -40,21 +52,51 @@ UserProfile.init(
     },
     mobileNumber: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      unique: true,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      field: "mobileNumber",
     },
     password: {
       type: DataTypes.STRING,
       allowNull: true,
     },
-  isVerified: {
+    isVerified: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
+    },
+    enabled: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      field: "isVerified",
+    },
+    role: {
+      type: DataTypes.ENUM("ROLE_USER", "ROLE_ADMIN"),
+      defaultValue: "ROLE_USER",
+    },
+    otp: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    otp_expiry: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    temprory_phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
   },
   {
     sequelize,
     tableName: "user_profiles",
     timestamps: true,
+    createdAt: "createdAt",
+    updatedAt: "updatedAt",
   }
 );
 
